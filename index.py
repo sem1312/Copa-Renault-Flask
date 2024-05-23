e597dd9c6a050c0f1a564a3e66d7e9755f1d081d
Christian <literariocba@gmail.com>
2024-05-23 11:23:32 -0300

    f

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
+
+# Lista de sponsors de ejemplo
+sponsors = ["Nike", "Adidas", "Coca-Cola", "Pepsi", "Visa", "Apple", "Samsung"]
 
 @app.route('/')
 def home():
@@ -16,6 +20,12 @@ def submit_contact():
     # Aquí procesas los datos del formulario
     nombre = request.form['nombre']
     return f'Formulario enviado por {nombre}'
+
+@app.route('/Sponsors')
+def sponsors_random():
+    # Mezclar los sponsors en orden aleatorio
+    random.shuffle(sponsors)
+    return render_template("spo.html", sponsors=sponsors)
 
 if __name__ == '__main__':
     app.run(debug=True)
